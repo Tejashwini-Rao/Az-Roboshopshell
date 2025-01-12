28a01b90920da722e6789100335af72d4963dd54
admin <polepalle.07@gmail.com>
2025-01-12 20:29:11 +0530

    added all components

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 
 rm -rf /app
@@ -18,7 +19,7 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.dveops77t.online </app/db/master-data.js
